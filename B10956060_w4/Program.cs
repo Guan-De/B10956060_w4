d21756a33258fe47977322cb00fec6439b3b7aa5
user <user@DESKTOP-6JBEF5K>
2022-03-14 10:01:18 +0800

    加入專案檔案。

---
+++ B10956060_w4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace B10956060_w4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
